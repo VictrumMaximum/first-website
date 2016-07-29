b8558e66094645f17b71e6a5aecdcebcb8636f68
Victor <victorpionescu@gmail.com>
2016-07-29 14:22:46 +0200

    Added ajax post function to send login credentials to server

--- login/login.js
+++ login/login.js
@@ -2,11 +2,28 @@ function login() {
 	var uname = $("#uname").val();
 	var pwd = $("#pwd").val();
 	
-	var registered = true;
-	
-	if(registered) {
-		loadIndex();
+	if(uname && pwd) {
+		var registered = true;
+		
+		$.ajax
+		({
+			type: "POST",
+			url: "http://localhost:3000/loginTry",
+			dataType: "json",
+			ContentType: "application/json",			
+			data: {uname, pwd},
+			success: function(data) {console.log(data);}
+		});
+		
+		if(registered) {
+			loadIndex();
+		}
+		
 	}
+	else {
+		window.alert("Fill in both inputs!");
+	}
+	
 }
 
 function loadIndex() {
--- node.js
+++ node.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var port = 3000;
 var server;
 
 app.use("/login", express.static(__dirname + "/login"));
 app.use("/home", express.static(__dirname + "/home"));
+
+app.post("/loginTry", urlencodedParser, function(req, res) {
+	console.log(req.body);
+	console.log(req.body.uname);
+	console.log(req.body.pwd);
+	
+})
 
 http.createServer(app).listen(port);
